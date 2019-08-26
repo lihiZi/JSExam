29a8aaa03aba89b4ff23f1c61cc1b8e374d5d147
lemonpeer <lihitalnz@gmail.com>
2019-08-26 15:58:44 +0300

    first commit

---
+++ table.js
@@ -0,0 +1,24 @@
+const contacts = [
+    {
+        name: "Adam Stanely",
+        email: "adam@gmail.com",
+        phone: "443-616-5716",
+        favorite: "true"
+    },
+    {
+        name: "Nate Bralt",
+        email: "nate@gmail.com",
+        phone: "443-818-5716",
+        favorite: "false"
+    },
+    {
+        name: "Rene Stanely",
+        email: "rene@gmail.com",
+        phone: "443-222-5716",
+        favorite: "true"
+    }
+
+
+]
+
+
